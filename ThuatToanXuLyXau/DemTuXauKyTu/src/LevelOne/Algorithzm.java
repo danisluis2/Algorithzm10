03721b46f088111439d8c3aa1894ffe9ce05e683
Nguyen Van Vuong <vuongluis@hotmail.com>
2016-11-23 09:32:51 +0700

    ok

---
+++ ThuatToanXuLyXau/DemTuXauKyTu/src/LevelOne/Algorithzm.java
@@ -0,0 +1,12 @@
+package LevelOne;
+
+public class Algorithzm {
+	
+	public static int Process(String temp){
+		return 0;
+	}
+	
+	public static void main(String[]args){
+		System.out.print("");
+	}
+}
